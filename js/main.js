c5ea9d0acbb65b1b45baad087a3e108251db06d8
LeoloFlrz <leoloflrzr@gmail.com>
2023-08-19 15:22:11 +0200

    created grid and accesed to dom elements

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const startButton = document.getElementById(startButton);
+const resetButton = document.getElementById(resetButton);
+
+const a1Cell = document.getElementById(a1);
+const a2Cell = document.getElementById(a2);
+const a3Cell = document.getElementById(a3);
+
+const b1Cell = document.getElementById(b1);
+const b2Cell = document.getElementById(b2);
+const b3Cell = document.getElementById(b3);
+
+const c1Cell = document.getElementById(c1);
+const c2Cell = document.getElementById(c2);
+const c3Cell = document.getElementById(c3);
+
+const players = [ player1, player2 ];
+
+startButton.addEventListener("click", () => {
+    const turno = player1;
+})
+
+
